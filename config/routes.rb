a407d4c9df1b20eabb0b6e1d497a4b9deb773f7a
thaiphuc2203 <thaiphuc2203@gmail.com>
2022-04-25 02:59:14 +0700

    add all project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'friendships/create'
+  get 'friendships/destroy'
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root 'welcome#index'
-  get 'my_portfodio', to: 'users#my_portfodio'
+  get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'users#search'
+  get 'my_friends', to: 'users#my_friends'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get my_portfodio" do
-    get users_my_portfodio_url
+  test "should get my_portfolio" do
+    get users_my_portfolio_url
     assert_response :success
   end
 
